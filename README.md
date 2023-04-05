64116fb4be745f78098f13306898f1651dd93734
[waynehq] <[814615396@qq.com]>
2023-04-05 17:29:36 +0800

    refactor: update readme.md

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Groot
-Groot install based on docker
+# Groot 
+Groot install based on docker.
+
+## when install finished
+
+```asm
+  cd ./scripts
+  ./start_groot_container.sh
+```
+
+<img src="./docs/Groot.png">
---
+++ scripts/start_groot_container.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+usage()
+{
+     echo "run.sh [-h|--help] [:VERSION] [COMMAND] [ARG...]"
+     exit 1
+}
+
+case "$1" in
+     :*)  tag=$1
+          shift
+          ;;
+
+     -h|--help)
+          usage
+          ;;
+esac
+
+#if [ -z "$DISPLAY" ]; then
+    #echo "ABORT: DISPLAY not set"
+    #exit 1
+#fi
+
+DOCKER_MAJOR_VERSION=$(docker version --format '{{.Client.Version}}' | cut -d. -f1)
+AMD_GPU=$(lspci | grep "VGA" | grep "AMD")
+if [ -n "$AMD_GPU" ]; then
+     readonly RUNTIME=""
+elif [ $DOCKER_MAJOR_VERSION -ge 19 ] && which nvidia-docker > /dev/null; then
+     readonly RUNTIME="--gpus all"
+else
+     readonly RUNTIME="--runtime=nvidia"
+fi
+
+echo $RUNTIME
+
+docker run -it --name groot-env --rm \
+  --user=$(id -u $USER):$(id -g $USER) \
+  --group-add=27 \
+  --workdir="/home/$USER" \
+  --volume="/home/$USER:/home/$USER" \
+  --volume="/etc/group:/etc/group:ro" \
+  --volume="/etc/passwd:/etc/passwd:ro" \
+  --volume="/etc/shadow:/etc/shadow:ro" \
+  --volume="/etc/sudoers.d:/etc/sudoers.d:rw" \
+  --volume="/etc/udev:/etc/udev" \
+  --privileged \
+  --device=/dev/ttyS0 \
+  $RUNTIME \
+  --net=host \
+  -e DISPLAY \
+  -e XAUTHORITY=/tmp/.Xauthority \
+  -v ${XAUTHORITY}:/tmp/.Xauthority \
+  -v /tmp/.X11-unix:/tmp/.X11-unix \
+  crp:groot bash \
+  -c "/usr/src/third_party/Groot/build/./Groot"
